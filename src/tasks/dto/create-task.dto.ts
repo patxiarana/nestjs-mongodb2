100a60048cc283b0cc90d2e9a37ca441baa7b971
unknown <patxiarana05@gmail.com>
2024-08-02 10:08:43 -0300

    class validator in dto and connect mongo in module

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,15 @@
-export interface CreateTaskDTO {
+import { IsString, IsBoolean, IsOptional,IsNotEmpty } from "class-validator";
+
+export class CreateTaskDTO {
+    @IsString()
+    @IsNotEmpty()
     title: string;
+   
+    @IsString()
+    @IsOptional()
     description?: string;
+   
+    @IsBoolean()
+    @IsOptional()
     done?:boolean;
 }
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,16 @@
-export interface UpdateTaskDTO {
+import { IsString,IsOptional,IsBoolean } from "class-validator";
+
+
+export class UpdateTaskDTO {
+    @IsString()
+    @IsOptional()
     title?: string;
+
+    @IsString()
+    @IsOptional()
     description?: string;
+    
+    @IsBoolean()
+    @IsOptional()
     done?:boolean;
 }
