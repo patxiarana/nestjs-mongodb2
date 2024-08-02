4e2eccb7b76cb68c4cb09dc44c69937dc80bb629
unknown <patxiarana05@gmail.com>
2024-08-02 09:35:41 -0300

    task schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-
+import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/taskdb'),
+    TasksModule],
 })
 export class AppModule {}
