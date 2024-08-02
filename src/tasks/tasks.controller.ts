264c23616dd8c04abd5192e071dffcf4a14b0c09
unknown <patxiarana05@gmail.com>
2024-08-02 10:15:01 -0300

    http methods

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller,Get,Post,Put,Delete } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor( tasksService: TasksService) {}
+
+    @Get()
+    findAll(){
+        return "Get all tasks" ; 
+    }
+    
+    @Get(':id')
+    findOne(){
+        return "Get  task" ; 
+    }
+    
+    @Post()
+    create(){
+        return "Create a new task" ; 
+    }
+
+    @Delete(':id')
+    delete(){
+        return "Delete a task" ; 
+    }
+
+    @Put(':id')
+    update(){
+        return "Update a task" ; 
+    }
+
+}
